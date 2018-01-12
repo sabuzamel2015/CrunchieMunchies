8d7c8d2df766b125a3f35633bb1d07968c288ffd
sabuzamel2015 <33916928+sabuzamel2015@users.noreply.github.com>
2018-01-11 23:23:42 -0500

    Update script.py

--- script.py
+++ script.py
@@ -2,24 +2,32 @@ import codecademylib
 
 import numpy as np
 
-import matplotlib as plt
 
+#generate CSV file
 calorie_stats = np.genfromtxt('cereal.csv', delimiter =",")
 
+#Finding the mean of calorie stats
 average_calorie = np.mean(calorie_stats)
 
+#Sort data in order to see if there is any potential outliers in our data
 sorting_data = np.sort(calorie_stats)
 
+#Find the median of calorie_stats. This is more accurate than mean for finding the average of a dataset
 median_calorie = np.median(calorie_stats)
 
+#This is the percentile of cereals that have over 100 calories. That is over 30 more than Yummy Corp's brand
 calorie_percentile = np.percentile(calorie_stats, 39)
 
+#This is the percentile of cereals that have 100 or more calories. That is 30 more than Yummy Corp's brand
 high_calorie = np.percentile(calorie_stats, 18)
 
+#This is the percentile that Crunchie_Munchies is in. Almost all of the cereals are less healthy than Yummy Corp's
 Crunchie_Munchies = np.percentile(calorie_stats, 4)
 
+#Finding the Standard Deviation of the dataset
 calorie_std = np.std(calorie_stats)
 
+#finding the difference of all the cereals within the first and third quartile
 calorie_interquartile = np.percentile(calorie_stats, 75) -np.percentile(calorie_stats, 25)
 
 print(average_calorie)
